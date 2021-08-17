08610fd6bee625b53ce73ac39969bcdba1ccb988
jscarlson <jcarlson238@gmail.com>
2021-08-17 03:11:15 -0400

    Fixed eval crops

--- core/data_loader.py
+++ core/data_loader.py
@@ -169,9 +169,9 @@ def get_test_loader(root, img_size=256, batch_size=32,
                              std=[0.5, 0.5, 0.5]),
     ])
     """
-    
+
     transform = transforms.Compose([
-        transforms.Resize(img_size),
+        transforms.RandomCrop(img_size),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5, 0.5, 0.5],
                              std=[0.5, 0.5, 0.5]),
